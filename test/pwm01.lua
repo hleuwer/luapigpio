9cd860261f3dafe87220bb205c59b534c005afc5
Herbert Leuwer <herbert.leuwer@gmx.de>
2018-09-10 20:01:04 +0200

    cleanup of tests

--- test/pwm01.lua
+++ test/pwm01.lua
@@ -14,17 +14,17 @@ gpio.setMode(pinp, gpio.INPUT)
 gpio.setMode(pout, gpio.OUTPUT)
 
 local count = 1
-local function alert(pin, level, tick)
+local function alert(pin, level, tick, udata)
    local _tick = gpio.tick()
    local delta = tick - last_tick
    last_tick = tick
-   print(string.format("alert cb %3d at %d (%4d us): pin=%d (ok=%s), level=%d, tick=%d us, delta=%.3f ms",
-                       count, _tick, _tick-tick, pin, tostring(pin==pinp), level, tick, delta/1000))
+   print(string.format("alert cb %3d at %d (%4d us): pin=%d (ok=%s), level=%d, tick=%d us, delta=%.3f ms duty=%d",
+                       count, _tick, _tick-tick, pin, tostring(pin==pinp), level, tick, delta/1000, udata.duty))
    count = count + 1
 end
 
 local freq = util.getNumber("PWM frequency:", 100)
-
+local udata = {duty=128}
 gpio.setPWMfrequency(pout, freq)
 printf("PWM frequency: %d", gpio.getPWMfrequency(pout))
 printf("Duty cycle range: %d", gpio.getPWMrange(pout))
@@ -32,7 +32,7 @@ printf("Duty cycle real range: %d", gpio.getPWMrealRange(pout))
 
 printf("Start PWM: duty cyle 128 (%.1f %%) ...", 128/256 * 100)
 io.write("Hit <return> ...") io.flush() io.read()
-gpio.setAlertFunc(pinp, alert)
+gpio.setAlertFunc(pinp, alert, udata)
 last_tick = gpio.tick()
 gpio.PWM(pout, 128)
 printf("duty cycle: %d", gpio.getPWMdutycycle(pout))
@@ -41,6 +41,7 @@ gpio.PWM(pout, 0)
 printf("duty cycle: %d", gpio.getPWMdutycycle(pout))
 
 wait(0.2)
+udata.duty=255
 printf("Start PWM: duty cyle 255 (%.1f %%) ...", 255/256 * 100)
 io.write("Hit <return> ...") io.flush() io.read()
 last_tick = gpio.tick()
@@ -51,6 +52,7 @@ gpio.PWM(pout, 0)
 printf("duty cycle: %d", gpio.getPWMdutycycle(pout))
 
 wait(0.2)
+udata.duty=1
 printf("Start PWM: duty cyle 1 (%.1f %%) ...", 1/256 * 100)
 io.write("Hit <return> ...") io.flush() io.read()
 last_tick = gpio.tick()
--- test/pwmhw01.lua
+++ test/pwmhw01.lua
@@ -12,12 +12,12 @@ local rc2 = gpio.setMode(pout, gpio.OUTPUT)
 
 local last_tick = gpio.tick()
 local count = 1
-local function alert(pin, level, tick)
+local function alert(pin, level, tick, udata)
    local _tick = gpio.tick()
    local delta = tick - last_tick
    last_tick = tick
-   print(string.format("alert cb %3d at %d (%4d us): pin=%d (ok=%s), level=%d, tick=%d us, delta=%.3f ms",
-                       count, _tick, _tick-tick, pin, tostring(pin==pinp), level, tick, delta/1000))
+   print(string.format("alert cb %3d at %d (%4d us): pin=%d (ok=%s), level=%d, tick=%d us, delta=%.3f ms duty=%d",
+                       count, _tick, _tick-tick, pin, tostring(pin==pinp), level, tick, delta/1000, udata.duty))
    count = count + 1
 end
 
@@ -25,12 +25,12 @@ end
 local freq = util.getNumber("PWM frequency [Hz]:", 100)
 local wait = util.getNumber("burst size [ms]:", 200)
 local dutymax = 1000000
-
+local udata = {duty = dutymax * 0.5}
 printf("Duty cycle range: %d", gpio.getPWMrange(pout))
 
 printf("Start PWM: duty cyle %d (%.1f %%) ...", dutymax * 0.5, (dutymax * 0.5) / dutymax * 100)
 io.write("Hit <return> ...") io.flush() io.read()
-gpio.setAlertFunc(pinp, alert)
+gpio.setAlertFunc(pinp, alert, udata)
 last_tick = gpio.tick()
 
 local rc = gpio.hardwarePWM(pout, freq, dutymax * 0.5)
@@ -38,6 +38,7 @@ gpio.wait(wait/1000)
 gpio.setMode(pout, gpio.INPUT)
 gpio.setMode(pout, gpio.OUTPUT)
 gpio.wait(wait/1000)
+udata.duty = dutymax * 0.99
 printf("Start PWM: duty cyle %d (%.1f %%) ...", dutymax * 0.99, (dutymax * 0.99) / dutymax * 100)
 io.write("Hit <return> ...") io.flush() io.read()
 last_tick = gpio.tick()
@@ -47,6 +48,7 @@ gpio.setMode(pout, gpio.INPUT)
 
 gpio.setMode(pout, gpio.OUTPUT)
 gpio.wait(wait/1000)
+udata.duty = dutymax * 0.01
 printf("Start PWM: duty cyle %d (%.1f %%) ...", dutymax * 0.01, (dutymax * 0.01) / dutymax * 100)
 io.write("Hit <return> ...") io.flush() io.read()
 last_tick = gpio.tick()
